45dd1c8e91277144dbb3fec50541498c5d163e38
williamNyongesa <marakawilliam@gmail.com>
2023-07-15 19:01:38 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(array) {
+    const winningGame = array.find(game => game.result === "W");
+    if (winningGame) {
+      return winningGame.year;
+    }else
+    return undefined;
+  }
